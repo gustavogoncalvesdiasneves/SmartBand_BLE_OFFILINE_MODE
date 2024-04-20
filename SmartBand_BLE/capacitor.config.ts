0abc5cd8635030be7cbf4c7b8538528673346daf
Zorin_OS_PG4 <ggddnn71@gmail.com>
2024-04-20 11:56:38 -0300

    Initial Commit

---
+++ SmartBand_BLE/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SmartBand_BLE',
+  webDir: 'www'
+};
+
+export default config;
